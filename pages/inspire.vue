790acaa3444adc4f8759c9eb85c4fc5b9921c469
Jessica.W <jessica.watson1@live.com>
2021-11-17 13:10:56 -0700

    added new post option

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,19 +1,24 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+  <div>
+    <v-row>
+      <v-col class="text-center">
+        <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+        <blockquote class="blockquote">
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-col>
+    </v-row>
+    <v-col>
+      <v-row>
+        <blockquote>Anything you can do I can do better.</blockquote>
+        <p>super cool person</p>
+        <v-btn elevation="2" fab raised small>B</v-btn>
+      </v-row>
     </v-col>
-  </v-row>
+  </div>
 </template>
